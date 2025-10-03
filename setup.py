75687fbe39c9dfcbde288f1982ecc79e0345b8cb
cmalbrec <38668527+cmalbrec@users.noreply.github.com>
2025-10-03 04:14:19 +0200

    fix typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ssl4eo_forest",
+    name="ssl4eo_eu_forest",
     version="1.0",
     description="TorchGeo-compatible dataset for SSL4EO-EU Forest segmentation",
     author="Conrad M Albrecht",
