d371c591a6b9e53c71e47c9a33aea9c9d5c5716b
cmalbrec <38668527+cmalbrec@users.noreply.github.com>
2025-10-03 04:13:31 +0200

    add setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ssl4eo_forest",
+    version="1.0",
+    description="TorchGeo-compatible dataset for SSL4EO-EU Forest segmentation",
+    author="Conrad M Albrecht",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "torchgeo",
+        "rasterio",
+        "geopandas",
+        "shapely",
+        "folium",
+        "requests",
+        "datasets",
+        "huggingface_hub"
+    ],
+    python_requires=">=3.8",
+)
