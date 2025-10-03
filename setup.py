c0c2290549587f63f0925f495572476877d573d0
cmalbrec <38668527+cmalbrec@users.noreply.github.com>
2025-10-03 04:52:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Conrad M Albrecht",
     packages=find_packages(),
     install_requires=[
-        "torch>=1.12",
+     "torch>=1.12",
         "torchgeo>=0.4",
         "rasterio>=1.3",
         "geopandas>=0.13",
@@ -16,7 +16,8 @@ setup(
         "requests>=2.28",
         "datasets>=2.14",
         "huggingface_hub>=0.17",
-        "tqdm>=4.66"
+        "tqdm>=4.66",
+        "numpy>=1.23"
     ],
     python_requires=">=3.8",
 )
