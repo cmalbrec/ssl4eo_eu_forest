355b6e0174130015b4196128d3899dc2f49eeef6
cmalbrec <38668527+cmalbrec@users.noreply.github.com>
2025-10-03 04:17:59 +0200

    add simple unit test

---
+++ tests/test_dataset.py
@@ -0,0 +1,6 @@
+from ssl4eo_forest.dataset import SSL4EOEUForestTG
+
+def test_dataset_loads():
+    ds = SSL4EOEUForestTG(root="./cache")
+    sample = ds[0]
+    assert "image" in sample and "mask" in sample
